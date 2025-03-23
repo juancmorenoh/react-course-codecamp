dd144cee3ee85be5b7d1cdcab4490f1be27ee49d
juan camilo <camilo.mro@gmail.com>
2025-03-23 23:47:47 +0000

    fix based in vite conf

--- react-tutorial/project6-endgame/vite.config.js
+++ react-tutorial/project6-endgame/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-course-codecamp"
+  //Absolutly necessary to deply vite
+  base: "/react-course-codecamp/react-tutorial/project6-endgame"
 })
